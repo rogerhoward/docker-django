270e71dc1c6a028528d004f7abec1f90b6ad6ef5
Roger Howard <rogerhoward@mac.com>
2017-09-12 19:36:11 -0700

    Mostly working

--- web/server.py
+++ web/server.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'hello'
+    return 'well hello'
 
 #------------------------------------------------#
 # System status endpoints                        #
